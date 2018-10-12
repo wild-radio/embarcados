b39bae3bdd4a5df3a2b962e19951afadf3717db6
Everton Barreto <kakivis1@gmail.com>
2018-10-11 21:08:03 -0300

    Teste saco cheio

--- main.py
+++ main.py
@@ -5,8 +5,6 @@ import numpy as np
 import cv2
 from time import sleep
 
-sensor = False
-timed_photo = False
 
 class Motors:
     def __init__(self, out1, out2):
@@ -52,7 +50,8 @@ class FileMonitor(threading.Thread):
         threading.Thread.__init__(self)
 
     def run(self):
-        global sensor
+        global motors_cam1
+        global motors_cam2
         while True:
             if os.path.exists(self.nome) and os.path.getmtime(self.nome) != self.last_modified:
                 self.last_modified = os.path.getmtime(self.nome)
@@ -60,10 +59,12 @@ class FileMonitor(threading.Thread):
                 f = open(self.nome, "r")
                 lines = f.read().splitlines()
                 print lines
-                Motors.set_angle1(lines[0])
-                Motors.set_angle2(lines[1])
+                motors_cam1.set_angle1(lines[0])
+                motors_cam1.set_angle2(lines[1])
                 sleep(2)
-                sensor = True
+                motors_cam2.set_angle1(lines[2])
+                motors_cam2.set_angle2(lines[3])
+                sleep(2)
                 f.close()
 
 
@@ -72,25 +73,21 @@ class Camera(threading.Thread):
         self.cam = cv2.VideoCapture(cam)  # starts cam capturing
         self.img_counter = 0  # picture_name counter
         threading.Thread.__init__(self)
+        GPIO.setup(3, GPIO.IN)
 
     def run(self):
-        global sensor
-        global timed_photo
         while True:
-            if sensor or timed_photo:
-                frame = self.cam.read()[1]
-                img_name = "opencv_frame_{}.png".format(self.img_counter)
-                cv2.imwrite(img_name, frame)
-                print("{} written!".format(img_name))
-                self.img_counter += 1
-                sensor = False
-                timed_photo = False
+            GPIO.wait_for_edge(3, GPIO.RISING)
+            frame = self.cam.read()[1]
+            img_name = "opencv_frame_{}.png".format(self.img_counter)
+            cv2.imwrite(img_name, frame)
+            print("{} written!".format(img_name))
+            self.img_counter += 1
 
 
-archive_monitor = FileMonitor("arquivo.txt")
-archive_monitor.start()
+motors_cam1 = Motors(38, 40)
+motors_cam2 = Motors(35, 37)
 file_monitor = FileMonitor("file.txt")
 file_monitor.start()
 camera_thread = Camera(0)
 camera_thread.start()
-motors_cam1 = Motors(0o3, 0o5)
