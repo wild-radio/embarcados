fc1f1a599ecd52d3a32c105889445f2fb4db29b1
Everton Barreto <kakivis1@gmail.com>
2018-10-16 16:33:48 -0300

    agora vai

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import RPi.GPIO as GPIO
 import numpy as np
 import cv2
 from time import sleep
+import time
 
 
 class Motors:
@@ -11,21 +12,21 @@ class Motors:
         # initialize
         GPIO.setwarnings(False)
         GPIO.setmode(GPIO.BOARD)
-        GPIO.setup(out1, GPIO.OUT)  # 0o3
-        GPIO.setup(out2, GPIO.OUT)  # 0o5
-        # setup frequency
-	self.pin1 = out1
-	self.pin2 = out2
-        self.pwm1 = GPIO.PWM(out1, 50)
-        self.pwm2 = GPIO.PWM(out2, 50)
-
-        # starts with 0% duty cycle
-        self.pwm1.start(0)
-        self.pwm2.start(0)
+        GPIO.setup(out1, GPIO.OUT)
+        GPIO.setup(out2, GPIO.OUT)
+        self.pin1 = out1
+        self.pin2 = out2
+        self.pwm1 = GPIO.PWM(out1, 50) # setup frequency
+        self.pwm2 = GPIO.PWM(out2, 50) # setup frequency
+        self.pwm1.start(0) # starts with 0% duty cycle
+        self.pwm2.start(0) # starts with 0% duty cycle
 
     # changes servo 1 angle
     def set_angle1(self, angle):
-        duty = int(angle) / 18 + 2
+        if angle < -60 or angle > 60:
+            return
+
+        duty = (90 + int(angle)) / 18 + 2
         GPIO.output(self.pin1, True)
         self.pwm1.ChangeDutyCycle(duty)
         sleep(1)
@@ -34,7 +35,10 @@ class Motors:
 
     # changes servo 2 angle
     def set_angle2(self, angle):
-        duty = int(angle) / 18 + 2
+        if angle < -60 or angle > 60:
+            return
+
+        duty = (90 + int(angle)) / 18 + 2
         GPIO.output(self.pin2, True)
         self.pwm2.ChangeDutyCycle(duty)
         sleep(1)
@@ -43,10 +47,10 @@ class Motors:
 
 
 class FileMonitor(threading.Thread):
-    def __init__(self, nome_do_arquivo):
-        self.nome = nome_do_arquivo
-        if os.path.exists(self.nome):
-            self.last_modified = os.path.getmtime(self.nome)
+    def __init__(self, file_name):
+        self.file_name = file_name
+        if os.path.exists(self.file_name):
+            self.last_modified = os.path.getmtime(self.file_name)
         else:
             self.last_modified = None
         threading.Thread.__init__(self)
@@ -54,40 +58,76 @@ class FileMonitor(threading.Thread):
     def run(self):
         global motors_cam1
         global motors_cam2
+        global cam1
+        global cam2
         while True:
-            if os.path.exists(self.nome) and os.path.getmtime(self.nome) != self.last_modified:
-                self.last_modified = os.path.getmtime(self.nome)
-                print('%s modified' % self.nome)
-                f = open(self.nome, "r")
+            if os.path.exists(self.file_name) and os.path.getmtime(self.file_name) != self.last_modified:
+                self.last_modified = os.path.getmtime(self.file_name)
+                print('%s modified' % self.file_name)
+                f = open(self.file_name, "r")
                 lines = f.read().splitlines()
                 print lines
-                motors_cam1.set_angle1(lines[0])
-                motors_cam1.set_angle2(lines[1])
-                motors_cam2.set_angle1(lines[2])
-                motors_cam2.set_angle2(lines[3])
+                if self.file_name == "~/.wildradio/config/principal.txt":
+                    cam1.active = (lines[0] == 1)  # type: bool
+                    cam1.periodic = (lines[1] == 1)  # type: bool
+                    cam1.sensor_flag = (lines[2] == 1)  # type: bool
+                    motors_cam1.set_angle1(lines[3])
+                    motors_cam1.set_angle2(lines[4])
+                    if lines[5] == 1:
+                        cam1.take_picture("~/.wildradio/pictures/confirmation_cam_1")
+                elif self.file_name == "~/.wildradio/config/alternativa.txt":
+                    cam2.active = (lines[0] == 1)   # type: bool
+                    cam2.periodic = (lines[1] == 1)  # type: bool
+                    cam2.sensor_flag = (lines[2] == 1)  # type: bool
+                    motors_cam2.set_angle1(lines[3])
+                    motors_cam2.set_angle2(lines[4])
+                    if lines[5] == 1:
+                        cam2.take_picture("~/.wildradio/pictures/confirmation_cam_2")
+
                 f.close()
 
 
 class Camera(threading.Thread):
-    def __init__(self, cam):
+    def __init__(self, cam, index, sensor_pin):
         self.cam = cv2.VideoCapture(cam)  # starts cam capturing
-        self.img_counter = 0  # picture_name counter
+        self.cam_index = index            # cam number to img name
+        self.sensor_pin = sensor_pin
+        self.active = False
+        self.periodic = False
+        self.timer_flag = False
+        self.sensor_flag = False
+        GPIO.setup(self.sensor_pin, GPIO.IN)
+        GPIO.add_event_detect(self.sensor_pin, GPIO.RISING)
+        self.timed_photo()
         threading.Thread.__init__(self)
-        GPIO.setup(3, GPIO.IN)
 
     def run(self):
         while True:
-            GPIO.wait_for_edge(3, GPIO.RISING)
-            frame = self.cam.read()[1]
-            img_name = "opencv_frame_{}.png".format(self.img_counter)
-            cv2.imwrite(img_name, frame)
-            print("{} written!".format(img_name))
-            self.img_counter += 1
+            if self.active:
+                if (GPIO.event_detected(self.sensor_pin) and self.sensor_flag) or (self.periodic and self.timer_flag):
+                    img_name = "cam{}_{}.png".format(self.cam_index, time.time())
+                    self.take_picture(img_name)
+                else:
+                    self.timer_flag = False
+
+    def take_picture(self, img_name):
+        frame = self.cam.read()[1]
+        cv2.imwrite(img_name, frame)
+        print("{} written!".format(img_name))
+
+    def timed_photo(self):
+        if self.periodic:
+            self.timer_flag = True
+        threading.Timer(60, self.timed_photo).start()
 
 
 motors_cam1 = Motors(40, 37)
 motors_cam2 = Motors(38, 35)
-file_monitor = FileMonitor("file.txt")
-file_monitor.start()
-camera_thread = Camera(-1)
-camera_thread.start()
+cam1 = Camera(0, 1, 3)
+cam1.start()
+cam2 = Camera(1, 2, 3)
+cam2.start()
+file_monitor1 = FileMonitor("~/.wildradio/config/principal.txt")
+file_monitor1.start()
+file_monitor2 = FileMonitor("~/.wildradio/config/alternativa.txt")
+file_monitor2.start()
