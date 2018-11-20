46389e83bbc487dc390ae12dd00552692d25f41d
Everton Barreto <kakivis1@gmail.com>
2018-11-20 20:17:33 -0200

    Fix no get_cams_ids para pegar direto o numero do device de video

--- lsusb.py
+++ lsusb.py
@@ -1,10 +1,18 @@
 import subprocess
 
 lsusb = subprocess.check_output(["lsusb"])
+ls_dev = subprocess.check_output(["ls", "/dev"])
 cam1_dev = None
 cam2_dev = None
 cam1_index = None
 cam2_index = None
+dev_ids = []
+
+for line in ls_dev.splitlines():
+    if "video" in line:
+        dev_ids.append(line[-1])
+
+dev_ids.sort()
 
 for line in lsusb.splitlines():
     words = line.split()
@@ -14,17 +22,17 @@ for line in lsusb.splitlines():
         cam2_dev = int(words[3].replace(":", ""))
 
 if cam1_dev and cam2_dev and cam1_dev > cam2_dev:
-    cam1_index = 1
-    cam2_index = 0
+    cam1_index = dev_ids[1]
+    cam2_index = dev_ids[0]
 elif cam1_dev and cam2_dev and cam1_dev < cam2_dev:
-    cam1_index = 0
-    cam2_index = 1
+    cam1_index = dev_ids[0]
+    cam2_index = dev_ids[1]
 elif cam1_dev:
     cam1_index = 0
 elif cam2_dev:
     cam2_index = 0
 
 print "cam1 index = {}".format(cam1_index)
-print "cam1 dev = {}".format(cam1_dev)
+print "cam1 usb dev = {}".format(cam1_dev)
 print "cam2 index = {}".format(cam2_index)
-print "cam2 dev = {}".format(cam2_dev)
+print "cam2 usb dev = {}".format(cam2_dev)
--- main.py
+++ main.py
@@ -196,10 +196,18 @@ class Camera(threading.Thread):
 
 def get_cams_ids():
     lsusb = subprocess.check_output(["lsusb"])
+    ls_dev = subprocess.check_output(["ls", "/dev"])
     cam1_dev = None
     cam2_dev = None
     cam1_index = None
     cam2_index = None
+    dev_ids = []
+
+    for line in ls_dev.splitlines():
+        if "video" in line:
+            dev_ids.append(line[-1])
+
+    dev_ids.sort()
 
     for line in lsusb.splitlines():
         words = line.split()
@@ -209,11 +217,11 @@ def get_cams_ids():
             cam2_dev = int(words[3].replace(":", ""))
 
     if cam1_dev and cam2_dev and cam1_dev > cam2_dev:
-        cam1_index = 1
-        cam2_index = 0
+        cam1_index = dev_ids[1]
+        cam2_index = dev_ids[0]
     elif cam1_dev and cam2_dev and cam1_dev < cam2_dev:
-        cam1_index = 0
-        cam2_index = 1
+        cam1_index = dev_ids[0]
+        cam2_index = dev_ids[1]
     elif cam1_dev:
         cam1_index = 0
     elif cam2_dev:
